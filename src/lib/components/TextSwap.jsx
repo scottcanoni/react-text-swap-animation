4c01c3c694780b9ddb46456c608552565a85b54d
Scott Canoni <scottcanoni@gmail.com>
2023-01-18 22:13:59 -0500

    Switch from node-sass-chokidar to sass, and cleanup of unused variables.  Build fix too.

--- src/lib/components/TextSwap.jsx
+++ src/lib/components/TextSwap.jsx
@@ -106,7 +106,7 @@ export default function TextSwap({ words, animationOptions }) {
                     }
                 }, forwardStartTime);
 
-                // Half way towards the destination, switch to the destination letter
+                // Halfway towards the destination, switch to the destination letter
                 setTimeout(() => {
                     if (swap.src.letter !== ' ' && swap.dest.letter === ' ') {
                         // do nothing
@@ -151,50 +151,50 @@ export default function TextSwap({ words, animationOptions }) {
     }, [lettersRefs1, lettersRefs2, loopAnimation, updateAnimation, randomReverseMax, randomReverseMin, randomStartMax, randomStartMin, waitToStart, words]);
 
     return (
-            <div className="text-swap">
-                <div className="word word-1 hidden">
-                    {
-                        [...words[0]].map((letter, i) => {
-                            // eslint-disable-next-line react/no-array-index-key
-                            return <span ref={lettersRefs1.current[i]} className="letter" key={`${i}${letter}`}>{letter}</span>;
-                        })
-                    }
-                </div>
-                <div className="word word-2 hidden">
-                    {
-                        [...words[1]].map((letter, i) => {
-                            // eslint-disable-next-line react/no-array-index-key
-                            return <span ref={lettersRefs2.current[i]} className="letter" key={`${i}${letter}`}>{letter}</span>;
-                        })
-                    }
-                </div>
-                <div className="word word-animation">
-                    {
-                        swapAnimations.map((renderedLetter, i) => {
-                            const { id, letter, playing, disappear, src, dest } = renderedLetter;
+        <div className="text-swap">
+            <div className="word word-1 hidden">
+                {
+                    [...words[0]].map((letter, i) => {
+                        // eslint-disable-next-line react/no-array-index-key
+                        return <span ref={lettersRefs1.current[i]} className="letter" key={`${i}${letter}`}>{letter}</span>;
+                    })
+                }
+            </div>
+            <div className="word word-2 hidden">
+                {
+                    [...words[1]].map((letter, i) => {
+                        // eslint-disable-next-line react/no-array-index-key
+                        return <span ref={lettersRefs2.current[i]} className="letter" key={`${i}${letter}`}>{letter}</span>;
+                    })
+                }
+            </div>
+            <div className="word word-animation">
+                {
+                    swapAnimations.map((renderedLetter) => {
+                        const { id, letter, playing, disappear, src, dest } = renderedLetter;
 
-                            let letterStyles = {};
-                            if (playing) {
-                                const left = `${dest.rect.x}px`;
-                                letterStyles = { left };
-                            }
-                            else {
-                                const left = `${src.rect.x}px`;
-                                letterStyles = { left };
-                            }
+                        let letterStyles;
+                        if (playing) {
+                            const left = `${dest.rect.x}px`;
+                            letterStyles = { left };
+                        }
+                        else {
+                            const left = `${src.rect.x}px`;
+                            letterStyles = { left };
+                        }
 
-                            if (disappear) {
-                                letterStyles.opacity = 0;
-                            }
+                        if (disappear) {
+                            letterStyles.opacity = 0;
+                        }
 
-                            return (
-                                    <span key={id} className="letter" style={letterStyles}>
+                        return (
+                            <span key={id} className="letter" style={letterStyles}>
                                 {letter}
                             </span>
-                            );
-                        })
-                    }
-                </div>
+                        );
+                    })
+                }
             </div>
+        </div>
     );
 }
